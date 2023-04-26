6b5ffb0ef790f5e4762824a5b9d22e6be78805b5
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-04-26 12:47:31 +0300

    feat: render create pairs

---
+++ js/components/createPairs.js
@@ -0,0 +1,46 @@
+import { createElement } from "../helper/createElement.js";
+
+export const createPairs = () => {
+    const pairs = createElement('section', {
+        className: 'card section-offset',
+    });
+
+    const container = createElement('div', {
+        className: 'container card__container',
+    });
+
+    const buttonReturn = createElement('button', {
+        className: 'card__return',
+        ariaLabel: 'Возврат к категориям',
+    });
+
+    const buttonCard = createElement('button', {
+        className: 'card__item',
+    });
+
+    const front = createElement('span', {
+        className: 'card__front',
+        textContent: 'one',
+    });
+    const back = createElement('span', {
+        className: 'card__back',
+        textContent: 'two',
+    });
+
+    buttonCard.append(front, back);
+    container.append(buttonReturn, buttonCard);
+    pairs.append(container);
+
+
+    const mount = data => {
+        app.append(pairs);
+    };
+
+    const unmount = () => {
+        pairs.remove();
+    }
+
+    return { buttonReturn, mount, unmount };
+
+};
+
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import { createCategory } from './components/createCategory.js';
 import { createEditCategory } from './components/createEditCategory.js';
 import { createHeader } from './components/createHeader.js';
+import { createPairs } from './components/createPairs.js';
 import { createElement } from './helper/createElement.js';
 import { fetchCards, fetchCategories } from './service/api.service.js';
 
@@ -11,9 +12,10 @@ const initApp = async () => {
     const headerObj = createHeader(headerParent);
     const categoryObj = createCategory(app);
     const editCategoryObj = createEditCategory(app);
+    const pairsObj = createPairs(app);
 
     const allSectionUnmount = () => {
-        [categoryObj, editCategoryObj].forEach(obj => obj.unmount());
+        [categoryObj, editCategoryObj, pairsObj].forEach(obj => obj.unmount());
 
     }
 
@@ -21,7 +23,7 @@ const initApp = async () => {
         e?.preventDefault();
         allSectionUnmount();
         const categories = await fetchCategories();
-
+        headerObj.updateHeaderTitle('Категории');
         if (categories.error) {
             app.append(createElement('p', {
                 className: 'server-error',
@@ -51,7 +53,22 @@ const initApp = async () => {
             editCategoryObj.mount(dataCards);
             return;
         }
+
+        if (target.closest('.category__del')) {
+            console.log('Удалить');
+            return;
+        }
+
+        if (categoryItem) {
+            const dataCards = await fetchCards(categoryItem.dataset.id);
+            allSectionUnmount();
+            headerObj.updateHeaderTitle(dataCards.title);
+            pairsObj.mount(dataCards);
+        }
     });
+
+
+    pairsObj.buttonReturn.addEventListener('click', renderIndex);
 };
 
 initApp();
