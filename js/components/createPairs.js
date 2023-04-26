bd47fe6904295ef532755a13ac928c2c9294e769
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-04-26 15:30:06 +0300

    feat: cards done

--- js/components/createPairs.js
+++ js/components/createPairs.js
@@ -1,4 +1,5 @@
 import { createElement } from "../helper/createElement.js";
+import { showAlert } from "./showAlert.js";
 
 export const createPairs = () => {
     const pairs = createElement('section', {
@@ -31,9 +32,49 @@ export const createPairs = () => {
     container.append(buttonReturn, buttonCard);
     pairs.append(container);
 
+    const cardControler = data => {
+
+        let index = 0;
+        front.textContent = data[index][0];
+        back.textContent = data[index][1];
+
+        const flipCard = () => {
+            buttonCard.classList.add('card__item_flipped');
+            buttonCard.removeEventListener('click', flipCard);
+            setTimeout(() => {
+                buttonCard.classList.remove('card__item_flipped');
+
+                setTimeout(() => {
+                    index++;
+
+                    if (index === data.length) {
+                        front.textContent = 'Конец';
+                        showAlert('Вернемся к началу!', 2000);
+
+                        setTimeout(() => {
+                            buttonReturn.click();
+                        }, 2000);
+                        return;
+                    }
+
+                    front.textContent = data[index][0];
+                    back.textContent = data[index][1];
+
+                    setTimeout(() => {
+                        buttonCard.addEventListener('click', flipCard);
+                    }, 200);
+                }, 100);
+            }, 1000);
+        };
+
+        buttonCard.addEventListener('click', flipCard);
+    }
+
+
 
     const mount = data => {
         app.append(pairs);
+        cardControler(data.pairs);
     };
 
     const unmount = () => {
